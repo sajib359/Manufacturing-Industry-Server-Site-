5083d67be4bbec2361def36b909ac61c274bb127
Sajib Hossain <sajib.sj359@gmail.com>
2022-05-28 20:39:49 +0600

    try to fixing heroku probelm

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello manufacturing industry!");
 });
 
 app.listen(port, () => {
