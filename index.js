c4327828ea1b3b0b60cc90d2b2a3bf46eae8fd83
Sajib Hossain <sajib.sj359@gmail.com>
2022-05-28 20:56:06 +0600

    again trying

--- index.js
+++ index.js
@@ -31,20 +31,7 @@ const client = new MongoClient(uri, {
 
 console.log(uri);
 
-// const verifyJWT = (req, res, next) => {
-//   const authHeader = req.headers.authorization;
-//   if (!authHeader) {
-//     return res.status(401).send({ message: "Unauthorized Access" });
-//   }
-//   const token = authHeader.split(" ")[1];
-//   jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
-//     if (err) {
-//       return res.status(403).send({ message: "Forbidden Access" });
-//     }
-//     req.decoded = decoded;
-//     next();
-//   });
-// };
+
 
 async function run() {
   try {
